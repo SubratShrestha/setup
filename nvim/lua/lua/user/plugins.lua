f7224a2913787d14a53031ddf62552b495aa6d74
Subrat Shrestha <subrat.shrestha3@gmail.com>
2021-12-27 11:47:22 +1100

    added toggle term

--- nvim/lua/lua/user/plugins.lua
+++ nvim/lua/lua/user/plugins.lua
@@ -87,6 +87,8 @@ return packer.startup(function(use)
 
   use "lewis6991/gitsigns.nvim" -- git indicators
 
+  use "akinsho/toggleterm.nvim" -- toggle terminal
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then

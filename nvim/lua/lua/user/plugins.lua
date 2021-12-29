313431307950348a1d19a9c7e8414dbd06c72c44
Subrat Shrestha <subrat.shrestha3@gmail.com>
2021-12-30 10:59:54 +1100

    add lualine

--- nvim/lua/lua/user/plugins.lua
+++ nvim/lua/lua/user/plugins.lua
@@ -89,7 +89,9 @@ return packer.startup(function(use)
 
   use "akinsho/toggleterm.nvim" -- toggle terminal
 
-  use "romgrk/barbar.nvim"
+  use "romgrk/barbar.nvim" -- buffers implemented as tabs 
+
+  use 'nvim-lualine/lualine.nvim' -- bottom bar
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then

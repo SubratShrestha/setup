9cbcac35916f6c4b22c9c06c584cb52435c78fee
Subrat Shrestha <subrat.shrestha3@gmail.com>
2022-01-18 18:41:00 +1100

    Remove friendly snippets and add custom

---
+++ nvim/lua/lua/user/snippets.lua
@@ -0,0 +1,49 @@
+local status_ok, ls = pcall(require, "luasnip")
+if not status_ok then
+	return
+end
+
+-- some shorthands...
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local l = require("luasnip.extras").lambda
+local r = require("luasnip.extras").rep
+local p = require("luasnip.extras").partial
+local m = require("luasnip.extras").match
+local n = require("luasnip.extras").nonempty
+local dl = require("luasnip.extras").dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local types = require("luasnip.util.types")
+local conds = require("luasnip.extras.expand_conditions")
+
+-- args is a table, where 1 is the text in Placeholder 1, 2 the text in
+-- placeholder 2,...
+local function copy(args)
+	return args[1]
+end
+
+ls.snippets = {
+	cpp = {
+		s("main", {
+			t("int main() {"),
+			t({ "", "\t" }),
+			i(0),
+			t({ "", "\treturn 0;" }),
+			t({ "", "}" }),
+		}),
+		s("std", {
+			t({ "#include<iostream>", "", "using namespace std;", "", "" }),
+			i(0),
+		}),
+        s("nl", {
+            t("<< '\\n'"),
+            i(0)
+        })
+	},
+}
